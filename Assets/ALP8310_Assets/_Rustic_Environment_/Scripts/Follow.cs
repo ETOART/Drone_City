8fa4da93d4ce2ec117d2f6c345a93197bacc33e7
sparkdesignn <79219776+sparkdesignn@users.noreply.github.com>
2023-10-25 23:08:12 +0400

    ыгььфкн

---
+++ Assets/ALP8310_Assets/_Rustic_Environment_/Scripts/Follow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Follow : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        if(player == null)
+        {
+            return;
+        }
+
+        transform.position = player.transform.position;
+    }
+}
