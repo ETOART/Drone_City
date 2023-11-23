0fedd16dd35bcae0f952fc420aeee088eb6fe4e7
danil matvienko <81968795+JanClopVandal@users.noreply.github.com>
2023-11-23 16:49:25 +0400

    Add 2dCompas

---
+++ Assets/Scripts/CompasController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompasController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform arrow;
+
+
+    private void LateUpdate()
+    {
+        //// Получите направление от стрелки к цели
+        //Vector3 direction = target.position - arrow.position;
+
+        //Quaternion rotation = Quaternion.LookRotation
+        //    (target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        //arrow.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+
+        //Vector3 direction = target.position - arrow.position;
+
+        // Вычислите угол между направлением и вектором (1, 0) в 2D пространстве
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        arrow.LookAt(target);
+        arrow.transform.eulerAngles = new Vector3(0, arrow.transform.eulerAngles.z, 0);
+
+
+
+
+    }
+}
