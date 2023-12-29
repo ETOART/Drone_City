8823e263f9707645eac1643bb894ec9f119bf3f8
danil matvienko <81968795+JanClopVandal@users.noreply.github.com>
2023-12-29 21:44:36 +0400

    Add statistic video

--- Assets/Scripts/S_00_VideoScreen_GameManager.cs
+++ Assets/Scripts/S_00_VideoScreen_GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
 using UnityEngine.InputSystem;
+using UnityEngine.Video;
 
 public class S_00_VideoScreen_GameManager : MonoBehaviour
 {
@@ -14,19 +15,33 @@ public class S_00_VideoScreen_GameManager : MonoBehaviour
     [SerializeField] private SessionController sessionController;
 
     [SerializeField] private InputActionReference _inputXAction = default;
+    [SerializeField] private InputActionReference _inputAAction = default;
 
     [SerializeField] private CanvasGroup _transitScreen;
+
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private List<VideoClip> clips;
+
+    private CanvasGroup playerAlpha;
+
+    private bool waitToChangeClip = false;
 
     private bool oneShotmethod = true;
     private void Start()
     {
         sessionController.isGame = false;
         LeanTween.alphaCanvas(_transitScreen, 0, 1f).setEaseLinear();
+        videoPlayer.loopPointReached += loopPointReached;
+        playerAlpha = videoPlayer.gameObject.GetComponent<CanvasGroup>();
+    }
+    private void OnDestroy()
+    {
+        videoPlayer.loopPointReached -= loopPointReached;
     }
 
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.Space) || _inputXAction.action.ReadValue<float>() != 0) && oneShotmethod )
+        if ((Input.GetKeyDown(KeyCode.Space) || _inputAAction.action.ReadValue<float>() != 0) && oneShotmethod )
         {
             oneShotmethod = false;
             SessionController.gameIsRegister = false;
@@ -41,6 +56,12 @@ public class S_00_VideoScreen_GameManager : MonoBehaviour
             LeanTween.alphaCanvas(_transitScreen, 1, 1f).setDelay(0.1f).setEaseLinear().setOnComplete(LoadScene);  
         }
 
+
+        if (((Input.GetKeyDown(KeyCode.C) || _inputXAction.action.ReadValue<float>() != 0) && oneShotmethod) && !waitToChangeClip)
+        {
+            StartStatVideo();
+        }
+
     }
 
     private void LoadScene()
@@ -48,5 +69,29 @@ public class S_00_VideoScreen_GameManager : MonoBehaviour
         SceneManager.LoadSceneAsync(scenename);
     }
    
+    private void StartStatVideo()
+    {
+        ShowNextVideo(clips[1]);
+        waitToChangeClip = true;
+    }
 
+    private void ShowNextVideo(VideoClip clip)
+    {
+        LeanTween.alphaCanvas(playerAlpha, 0, 1f).setEaseLinear().setOnComplete((() =>
+        {
+            videoPlayer.clip = clip;
+            videoPlayer.Play();
+            LeanTween.alphaCanvas(playerAlpha, 1, 1.5f).setDelay(0.5f).setEaseLinear();
+
+        }));
+
+
+    }
+    public void loopPointReached(VideoPlayer source){
+        if (waitToChangeClip)
+        {
+            ShowNextVideo(clips[0]);
+            waitToChangeClip = false;
+        }
+    }
 }
